1f0b0fe2f6cae1922e61d92663892fb5c6fb5f53
osisamkay <osisami.oj@gmail.com>
2019-10-19 09:51:09 +0100

    rgth

--- src/Components/FormComponent.js
+++ src/Components/FormComponent.js
@@ -1,5 +1,5 @@
 import React,{Fragment} from 'react';
-import { Form, Input,  } from 'antd';
+import { Form, Input } from 'antd';
 import { FormItemLayout} from "../Components/Formlayout"
 // ............form layout...............
 
