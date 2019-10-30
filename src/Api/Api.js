596fc073f06d2be636589c8739d83a095920f1d6
osisamkay <osisami.oj@gmail.com>
2019-10-30 21:51:09 +0100

    hgubh

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -2,8 +2,8 @@
 export const Data =async data => {
     try {
         const response = await fetch('https://us-central1-enye-reduxx.cloudfunctions.net/users', {
-            method: 'POST', // or 'PUT'
-            body: JSON.stringify(data), // data can be `string` or {object}!
+            method: 'POST', 
+            body: JSON.stringify(data), 
             headers: {
                 'Content-Type': 'application/json'
             }
